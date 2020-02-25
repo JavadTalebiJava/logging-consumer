eb0efef2bcbae75a58d1d3555f6b42d18c382864
JavadTalebi <cevat.talebi@gmail.com>
2020-02-26 02:10:19 +0300

    add Person listener(consumer)
    Signed-off-by: JavadTalebi <cevat.talebi@gmail.com>

--- src/main/java/com/javadtalebi/cloudstream/loggingconsumer/LoggingConsumerApplication.java
+++ src/main/java/com/javadtalebi/cloudstream/loggingconsumer/LoggingConsumerApplication.java
@@ -1,7 +1,11 @@
 package com.javadtalebi.cloudstream.loggingconsumer;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Consumer;
 
 @SpringBootApplication
 public class LoggingConsumerApplication {
@@ -9,5 +13,35 @@ public class LoggingConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(LoggingConsumerApplication.class, args);
     }
+
+    /*
+    You can write a Spring Cloud Stream application by simply writing functions and exposing them as `@Bean`s.
+    You can also use Spring Integration annotations based configuration or Spring Cloud Stream annotation based configuration,
+    although starting with spring-cloud-stream 3.x we recommend using functional implementations.
+   */
+
+    @Bean
+    public Consumer<Person> log() {
+        return person -> {
+            System.out.println("Received: " + person);
+        };
+    }
+
+    public static class Person {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String toString() {
+            return this.name;
+        }
+    }
+
 
 }
--- src/test/java/com/javadtalebi/cloudstream/loggingconsumer/LoggingConsumerApplicationTests.java
+++ src/test/java/com/javadtalebi/cloudstream/loggingconsumer/LoggingConsumerApplicationTests.java
@@ -3,11 +3,9 @@ package com.javadtalebi.cloudstream.loggingconsumer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class LoggingConsumerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
