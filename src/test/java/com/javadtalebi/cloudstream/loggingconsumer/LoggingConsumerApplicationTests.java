922f8e028c62e27195a91ebb36fb34ea7de7f107
JavadTalebi <cevat.talebi@gmail.com>
2020-02-26 00:35:40 +0300

    2nd commit

---
+++ src/test/java/com/javadtalebi/cloudstream/loggingconsumer/LoggingConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javadtalebi.cloudstream.loggingconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
